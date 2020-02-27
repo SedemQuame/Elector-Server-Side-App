68f44e615d421711e473df1ffda2eafcf4eea43c
SedemQuame <sedem.amekpewu.3@gmail.com>
2020-02-27 06:33:35 +0000

    working stuff

--- routes/election.route.js
+++ routes/election.route.js
@@ -9,7 +9,7 @@ module.exports = app => {
     app.route('/create_election').post(election.createElection);    
 
     // get election result
-    app.route('/get_election').post(election.getElectionResult);
+    app.route('/get_election').get(election.getElectionResult);
 
     // get election result by id
     app.route('/get_election_by_id').post(election.getElectionResultById);
@@ -23,22 +23,4 @@ module.exports = app => {
     // updating election results
     app.route('/update_election').post(election.updateResults);
 
-    
-    // election creation route
-    app.route('/create_election').get(election.createElection);    
-
-    // get election result
-    app.route('/get_election').get(election.getElectionResult);
-
-    // get election result by id
-    app.route('/get_election_by_id').get(election.getElectionResultById);
-
-    // delete current election
-    app.route('/remove_election').get(election.removeElection);
-
-    // delete all the current election
-    app.route('/remove_all_elections').get(election.removeAllElection);
-
-    // updating election results
-    app.route('/update_election').get(election.updateResults);
 };
